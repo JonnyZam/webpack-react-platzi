176eaac2482cfb5b010641954b47174dfbeb49f5
Jonathan Zamora <jonathan.zamora12@gmail.com>
2023-04-26 16:36:28 -0400

    Se modifica archivo webpack.config.js por error typo en filename de output, aparte se agrega mejora en archivo babel, configuracion de scripts de production y development

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,10 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filname: 'bundle.js',
+        filename: 'bundle.js',
     },
     resolve: {
-        extensions: ['.js', '.jsx']
+        extensions: ['.js', '.jsx'],
     },
     module: {
         rules:[
